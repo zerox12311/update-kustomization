c2769fb59b32d9cc204cd62a3110c5935e565698
LuxProtoss <zerox12311@gmail.com>
2024-01-16 17:51:23 +0800

    feat: 🎸 add subgroup

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,26 +1,36 @@
 #!/bin/sh
 # apk update && apk add --no-cache git && apk add --no-cache openssh
-if [ -z "$SSH_KEY" ]
+if [ -z "$PLUGIN_SSH_KEY" ]
 then
-    GIT_REPO_URI="https://$GIT_TOKEN@$MANIFEST_HOST/${MANIFEST_NAMESPACE:-${MANIFEST_USER}}/$MANIFEST_REPO.git"
+    # if has MANIFEST_GROUP, add to GIT_REPO_URI
+    if [ -n "$PLUGIN_MANIFEST_GROUP" ]
+    then
+        GIT_REPO_URI="https://$PLUGIN_GIT_TOKEN@$PLUGIN_MANIFEST_HOST/${PLUGIN_MANIFEST_NAMESPACE:-${PLUGIN_MANIFEST_USER}}/${PLUGIN_MANIFEST_GROUP}/${PLUGIN_MANIFEST_REPO}.git"
+    else
+        GIT_REPO_URI="https://$PLUGIN_GIT_TOKEN@$PLUGIN_MANIFEST_HOST/${PLUGIN_MANIFEST_NAMESPACE:-${PLUGIN_MANIFEST_USER}}/${PLUGIN_MANIFEST_REPO}.git"
+    fi
 else
-    mkdir -p ~/.ssh && echo $SSH_KEY | base64 -d > ~/.ssh/id_rsa && chmod 700 ~/.ssh/id_rsa && ssh-keyscan $MANIFEST_HOST >> ~/.ssh/known_hosts
-    GIT_REPO_URI="ssh://git@$MANIFEST_HOST/${MANIFEST_NAMESPACE:-${MANIFEST_USER}}/$MANIFEST_REPO.git"
+    mkdir -p ~/.ssh && echo $PLUGIN_SSH_KEY | base64 -d > ~/.ssh/id_rsa && chmod 700 ~/.ssh/id_rsa && ssh-keyscan $PLUGIN_MANIFEST_HOST >> ~/.ssh/known_hosts
+    GIT_REPO_URI="ssh://git@$PLUGIN_MANIFEST_HOST/${PLUGIN_MANIFEST_NAMESPACE:-${PLUGIN_MANIFEST_USER}}/$PLUGIN_MANIFEST_REPO.git"
 fi
 
-rm -rf $MANIFEST_REPO && git clone $GIT_REPO_URI
+echo "$PLUGIN_MANIFEST_GROUP"
+echo "git clone $GIT_REPO_URI"
 
-cd $MANIFEST_REPO
-git checkout $MANIFEST_BRANCH
-cd $KUSTOMIZATION
+rm -rf $PLUGIN_MANIFEST_REPO && git clone $GIT_REPO_URI
 
-for IMAGE in $(echo $IMAGES | sed "s/,/ /g")
+cd $PLUGIN_MANIFEST_REPO
+git checkout $PLUGIN_MANIFEST_BRANCH
+cd $PLUGIN_KUSTOMIZATION
+
+for IMAGE in $(echo $PLUGIN_IMAGES | sed "s/,/ /g")
 do
-    echo "kustomize edit set image $IMAGE:$IMAGE_TAG"
-    kustomize edit set image $IMAGE:$IMAGE_TAG
+    echo "kustomize edit set image $IMAGE:$PLUGIN_IMAGE_TAG"
+    kustomize edit set image $IMAGE:$PLUGIN_IMAGE_TAG
 done
 
-git config --global user.name $MANIFEST_USER
-git config --global user.email $MANIFEST_USER_EMAIL
-git add . && git commit --allow-empty -m "🚀 update to ${IMAGE_TAG}"
+git config --global user.name $PLUGIN_MANIFEST_USER
+git config --global user.email $PLUGIN_MANIFEST_USER_EMAIL
+git add . && git commit --allow-empty -m "🚀 update to ${PLUGIN_IMAGE_TAG}"
 git push $GIT_REPO_URI
+
